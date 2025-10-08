861524ce6e87ffb55eeb786dff512d585988a9b0
FelipeFabiani <felipefabiani@gmail.com>
2025-10-08 23:18:44 +0100

    Establishing Connections between Services

--- 02-DataPersistence/10-CodeAndDeployNotesAppDockerCompose/notebooks-backend/src/server.js
+++ 02-DataPersistence/10-CodeAndDeployNotesAppDockerCompose/notebooks-backend/src/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/api/notebooks', (req, res) => {
     res.send('Hello from Notebooks Backend! new message');
 });
 
