bde9aca3b87b664dbf05c32383eccd0e764827c2
FelipeFabiani <felipefabiani@gmail.com>
2025-10-08 15:40:20 +0100

    Dockerizing the Notebooks Backend

--- 02-DataPersistence/10-CodeAndDeployNotesAppDockerCompose/notebooks-backend/src/server.js
+++ 02-DataPersistence/10-CodeAndDeployNotesAppDockerCompose/notebooks-backend/src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Notebooks Backend!');
+    res.send('Hello from Notebooks Backend! new message');
 });
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
