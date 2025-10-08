3f11b580e4143a74c742e3db98b2b7d2fa10c9dd
FelipeFabiani <felipefabiani@gmail.com>
2025-10-08 16:59:48 +0100

    Setting Up Docker Compose for Notes Services

--- 02-DataPersistence/10-CodeAndDeployNotesAppDockerCompose/notes-backend/src/server.js
+++ 02-DataPersistence/10-CodeAndDeployNotesAppDockerCompose/notes-backend/src/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello from Notes Backend!');
 });
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+
+console.log('Connecting to MongoDB...');
+console.log('Using connection string:', process.env.DB_URL);
+// MongoDB connection
+mongoose.connect(process.env.DB_URL)
+.then(() => {
+    console.log('MongoDB connected')
+    app.listen(port, () => {
+        console.log(`Notes API running on port ${port}`);
+    });
+})
+.catch(err => console.error('MongoDB connection error:', err));
+
+
