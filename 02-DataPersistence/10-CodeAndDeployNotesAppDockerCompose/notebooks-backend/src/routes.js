72f61faf2ee67c15cae27533f37349ac4c636e98
FelipeFabiani <felipefabiani@gmail.com>
2025-10-09 11:51:12 +0100

    Updating and Deleting Notebooks

--- 02-DataPersistence/10-CodeAndDeployNotesAppDockerCompose/notebooks-backend/src/routes.js
+++ 02-DataPersistence/10-CodeAndDeployNotesAppDockerCompose/notebooks-backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const notebookRouter = express.Router();
 const { AppError } = require('./appError');
 const { Notebook } = require('./models.js');
+const { default: mongoose } = require('mongoose');
 
 // Create new notebook: POST /api/notebooks
 notebookRouter.post('/', async (req, res, next) => {
@@ -35,21 +36,63 @@ notebookRouter.get('/', async (req, res, next) => {
 });
 
 // Get a specific notebook by ID: GET /api/notebooks/:id
-notebookRouter.get('/:id', (req, res) => {
+notebookRouter.get('/:id', async (req, res, next) => {
     const { id } = req.params;
-    res.send(`Get notebook with ID: ${id}`);
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return next(new AppError('Invalid notebook ID', 400));
+        }
+
+        const notebook = await Notebook.findById(id);
+        if (!notebook) {
+            return next(new AppError('Notebook not found', 404));   
+        }
+        return res.status(200).json({ data: notebook });
+    } catch (error) {
+        next(error);
+    }
 });
 
 // Update a specific notebook by ID: PUT /api/notebooks/:id
-notebookRouter.put('/:id', (req, res) => {
-    const { id } = req.params;
-    res.send(`Update notebook with ID: ${id}`);
+notebookRouter.put('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return next(new AppError('Invalid notebook ID', 400));
+        }
+        
+        const { name, description } = req.body;
+        const updatedNotebook = await Notebook.findByIdAndUpdate(
+            id,
+            { name, description },
+            { new: true }
+        );
+        if (!updatedNotebook) {
+            return next(new AppError('Notebook not found', 404));   
+        }
+        return res.status(200).json({ data: updatedNotebook });
+    } catch (error) {
+        next(error);
+    }
 });
 
 // Delete a specific notebook by ID: DELETE /api/notebooks/:id
-notebookRouter.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    res.send(`Delete notebook with ID: ${id}`);
+notebookRouter.delete('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return next(new AppError('Invalid notebook ID', 400));
+        }
+
+        const deletedNotebook = await Notebook.findByIdAndDelete(id);
+        if (!deletedNotebook) {
+            return next(new AppError('Notebook not found', 404));   
+        }
+        // return res.status(200).json({ data: deletedNotebook });
+        return res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
 });
 
 
